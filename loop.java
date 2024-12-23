3671e6312e686d7bdd26e31f086b7ee6991c40a5
kuntal1007 <108316608+kuntal1007@users.noreply.github.com>
2024-12-23 17:14:01 -0600

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,66 @@
+// 1.For loop
+
+// public class loop {
+//     public static void main(String[] args) {
+//         for (int i = 0; i>4; i++){
+//             System.out.println("hello world");
+//         }
+//     }
+    
+// }
+
+
+//2. while loop
+
+// public class loop{
+//     public static void main(String[] args) {
+//         int i=0;
+//         while(i<11){
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+
+// 3. do while loop
+
+// public class loop{
+//     public static void main(String[] args) {
+//         int i = 0;
+//         do { 
+//            System.out.println(i);
+//            i++; 
+//         } while (i<10);
+//     }
+// }
+
+// 4. sum of n number using loop
+
+// import java.util.*;
+// public class loop{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int sum = 0;
+//         for(int i = 1; i<=n; i++){
+//             sum = sum + i;
+            
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+
+// 5.print the table of any number
+
+// import java.util.*;
+// public class loop{
+//     public static void main(String[] args) {
+//         Scanner sc  = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for(int i= 1; i<=10; i++){
+//             System.out.println(i*n);
+//         }      
+//     }
+// }
